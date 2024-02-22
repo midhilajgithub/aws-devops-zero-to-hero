70b7bba46c3cd8d134965ced69e36ba3524861cf
midhilajgithub <127943471+midhilajgithub@users.noreply.github.com>
2024-02-22 17:38:17 +0530

    configured the start container file with login to ECR

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Authenticate Docker to ECR
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 271136683822.dkr.ecr.ap-south-1.amazonaws.com
+
 # Pull the Docker image from Docker Hub
 docker pull simple_python_app/271136683822.dkr.ecr.ap-south-1.amazonaws.com/simple_python_app:latest
 

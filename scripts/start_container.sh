fda5a6bba8712d4810d7679d2842bd94f12f3422
midhilajgithub <127943471+midhilajgithub@users.noreply.github.com>
2024-02-22 16:31:43 +0530

    start_container.sh changed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull simple_python_app/271136683822.dkr.ecr.ap-south-1.amazonaws.com/simple_python_app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 simple_python_app/271136683822.dkr.ecr.ap-south-1.amazonaws.com/simple_python_app:latest

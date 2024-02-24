8b3ea3c797fba1c8eabe7be68df817efb99de704
midhilajgithub <127943471+midhilajgithub@users.noreply.github.com>
2024-02-24 22:35:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -13,10 +13,10 @@ DOCKER_PASSWORD=$(echo $DOCKER_CREDENTIALS | jq -r .password)
 DOCKER_URL=$(echo $DOCKER_CREDENTIALS | jq -r .url)
 
 # Authenticate Docker to ECR
-aws ecr get-login-password --region ap-south-1 | docker login --username "$DOCKER_USERNAME" --password-stdin "$DOCKER_URL"
+aws ecr get-login-password --region ap-south-1 | docker login --username-stdin --password-stdin "$DOCKER_URL"
 
 # Pull the Docker image from Docker Hub
-docker pull simple_python_app/"$DOCKER_URL"
+docker pull "$DOCKER_URL"/simple_python_app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 simple_python_app/"$DOCKER_URL"
+docker run -d -p 5000:5000 "$DOCKER_URL"/simple_python_app:latest

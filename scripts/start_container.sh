ec26877d17eb5e6b3b73dc2a5282e05bf94751c5
midhilajgithub <127943471+midhilajgithub@users.noreply.github.com>
2024-02-24 19:01:37 +0530

    ECR login

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+DOCKER_CREDENTIALS=$(aws secretsmanager get-secret-value --secret-id docker_Secrets_manager_credentials --region ap-south-1 --output json | jq -r .SecretString)
+DOCKER_USERNAME=$(echo $DOCKER_CREDENTIALS | jq -r .username)
+DOCKER_PASSWORD=$(echo $DOCKER_CREDENTIALS | jq -r .password)
+DOCKER_URL=$(echo $DOCKER_CREDENTIALS | jq -r .url)
+
 # Authenticate Docker to ECR
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 271136683822.dkr.ecr.ap-south-1.amazonaws.com
+aws ecr get-login-password --region ap-south-1 | docker login --username "$DOCKER_USERNAME" --password-"$DOCKER_PASSWORD" "$DOCKER_URL"
 
 # Pull the Docker image from Docker Hub
-docker pull simple_python_app/271136683822.dkr.ecr.ap-south-1.amazonaws.com/simple_python_app:latest
+docker pull simple_python_app/"$DOCKER_URL"
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 simple_python_app/271136683822.dkr.ecr.ap-south-1.amazonaws.com/simple_python_app:latest
+docker run -d -p 5000:5000 simple_python_app/"$DOCKER_URL"
